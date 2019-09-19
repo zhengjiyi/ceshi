061a2719d6ee929d0a645c7aca4c6b72b6f03fd3
z1976718128 <1976718128@qq.com>
2019-09-19 18:06:02 +0800

    frist

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+     <input type="" :placeholder="this.msg"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg:""
+    }
+  },
+  mounted() {
+  	    let than = this;
+        than.axios.get("/api/newData").then((res) =>{
+        		than.msg=res.data.dataInfo.state;
+            console.log(than.msg)
+        })
+        
+   },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
